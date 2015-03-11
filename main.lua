556a0f4ccf17705e4c62b43b7fab5c413caaba8d
EntranceJew <EntranceJew@gmail.com>
2015-03-11 10:31:13 -0400

    Basic program loop.

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-print("hello I love you")
+local answer
+while true do
+	io.write("Do you love me (y/n)? ")
+	answer=io.read()
+	if answer == "y" then
+		io.write("I love you too.\n")
+	elseif answer == "n" then
+		io.write("That is okay.\n")
+		break
+	else
+		io.write("I don't understand.\n")
+	end
+	io.flush()
+end
