f0d9cbb8c7054da78799eb3a0a04de874591c715
EntranceJew <EntranceJew@gmail.com>
2015-04-13 20:50:12 -0400

    Remember when I said I wouldn't make it depend on love? I lied.
    It's the only luajit environment I know of and I can't figure out how to get luarocks to play nicely on windows.

--- main.lua
+++ main.lua
@@ -1,27 +1,40 @@
-require("keys")
+-- Example: Imgurlua
 local imgurlua = require("imgurlua")
 local client, answer, items
+require("keys")
+local results = ''
+function xprint(...)
+	local var={...}
+	results = results .. "\n" .. table.concat(var, '\n')
+end
 
-while true do
-	io.write("[d]ebug, [r]un, [e]xit, [m]oral support: ")
-	answer=io.read()
-	if answer == "d" then
+function love.load(arg)
+	love._openConsole()
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw()
+	love.graphics.printf(results, 10, 10, 0, "left")
+end
+
+function love.keypressed(const, code)
+	if const == 'd' then
 		debug.debug()
-	elseif answer == "r" then
+	elseif const == 'r' then
 		client = ImgurClient:new(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN)
 		
 		items = client:gallery()
 		for _,item in ipairs(items) do
-			print(_, item.id, item.account_url, item.title)
+			xprint(_, item.id, item.account_url, item.title)
 		end
-		print("GALLERY DELIVERED")
-	elseif answer == "e" then
-		io.write("Goodbye.")
-		break
-	elseif answer == "m" then
-		io.write("You're doing a great job.\n")
-	else
-		io.write("PARITY ERROR")
+		xprint("GALLERY DELIVERED")
+	elseif const == 'u' then
+		client = ImgurClient:new(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN)
+		
+		local juk = client:upload_from_path('hamster.png')
+		print_r(juk)
 	end
-	io.flush()
 end
